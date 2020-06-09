5433d7ea684326f0d5c596b3212a0ba31eace13c
Rex-Loyer <rexloyer@gmail.com>
2020-06-09 12:28:37 -0500

    require queries file, fix http action and implement usage of req.body
    Co-authored-by: Kody Maus <auto.mausx@gmail.com>
    Co-authored-by: Tim Black <timothyrblack@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 9000;
 const path = require('path');
-/*TODO: import queries
-const queries = require(../)
-*/
+const queries = require('./database/queries')
+
+
+//TODO: set correct HTTP actions, not all will be get requests
 
 // open up CORS
 app.use((_, res, next) => {
@@ -16,70 +17,76 @@ app.use((_, res, next) => {
 app.use(express.static(path.join(__dirname, "../client/public")))
 
 app.get('/api/findUser', function (req, res) {
-    queries.findUser(), (err,data) => {
+  const username = req.body.username;
+  queries.findUser(username, (err,data) => {
       if (err) {
         console.log("error finding user in server:", err)
         res.send("error finding user in server")
       }
       console.log("user found!");
       res.send(data);
-    }
+    })
   });
 
-  //front end decide way to pass data req?
-  app.get('/api/createUser', function (req, res) {
-    queries.createUser(), (err,data) => {
+  app.post('/api/createUser', function (req, res) {
+    const user = req.body.user;
+    queries.createUser(user, (err,data) => {
       if (err) {
         console.log("error creating user in route:", err)
         res.send("error getting all users data in server")
       }
       console.log("user created!");
       res.send(data);
-    }
+    })
   });
 
-  app.get('/api/updateUserConvos', function (req, res) {
-    queries.updateUserConvos(), (err,data) => {
+  app.put('/api/updateUserConvos', function (req, res) {
+    const username = req.body.username;
+    queries.updateUserConvos(username, convoId, (err,data) => {
       if (err) {
         console.log("error updating user convos in server:", err)
         res.send("error updating user convos in server")
       }
       console.log("user convo updated!");
       res.send(data);
-    }
+    })
   });
 
   app.get('/api/getConvo', function (req, res) {
-    queries.getConvo(), (err,data) => {
+    const convoId = req.body.convoId;
+    queries.getConvo(convoId, (err,data) => {
       if (err) {
         console.log("error getting convo in server:", err)
         res.send("error getting convo in server")
       }
       console.log("convo gotten!");
       res.send(data);
-    }
+    })
   });
-  //TODO: HANDLE DATA input, pass to querey
-  app.get('/api/createConvo', function (req, res) {
-    queries.createConvo(), (err,data) => {
+
+  app.post('/api/createConvo', function (req, res) {
+    const convo = req.body.convo;
+    queries.createConvo(convo, (err,data) => {
       if (err) {
         console.log("error creating convo in server:", err)
         res.send("error creating convo in server")
       }
       console.log("convo created!");
       res.send(data);
-    }
+    })
   });
 
-  app.get('/api/updateMessages', function (req, res) {
-    queries.updateMessages(), (err,data) => {
+  app.put('/api/updateMessages', function (req, res) {
+    const convoId = req.body.convoId;
+    const messageData = req.body.messageData;
+    queries.updateMessages(convoId, messageData, (err,data) => {
       if (err) {
         console.log("error updating messeges in server:", err)
         res.send("error updating messages in server")
       }
       console.log("messages updated!");
       res.send(data);
-    }
+    })
   });
 
 app.listen(port, function() {
