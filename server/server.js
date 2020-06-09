1101e0fbc585cad0af533bc0934bd06a93ab60ec
Rex-Loyer <rexloyer@gmail.com>
2020-06-09 12:08:36 -0500

    Finish routes and sever config (except for req.body processing)
    Co-authored-by: Kody Maus <auto.mausx@gmail.com>
    Co-authored-by: Tim Black <timothyrblack@gmail.com>

--- server/server.js
+++ server/server.js
@@ -16,69 +16,70 @@ app.use((_, res, next) => {
 app.use(express.static(path.join(__dirname, "../client/public")))
 
 app.get('/api/findUser', function (req, res) {
-    queries.findUser(err,data) => {
+    queries.findUser(), (err,data) => {
       if (err) {
         console.log("error finding user in server:", err)
         res.send("error finding user in server")
       }
       console.log("user found!");
       res.send(data);
-    })
+    }
   });
 
   //front end decide way to pass data req?
   app.get('/api/createUser', function (req, res) {
-    queries.createUser(err,data) => {
+    queries.createUser(), (err,data) => {
       if (err) {
         console.log("error creating user in route:", err)
         res.send("error getting all users data in server")
       }
       console.log("user created!");
       res.send(data);
-    })
+    }
   });
 
   app.get('/api/updateUserConvos', function (req, res) {
-    queries.updateUserConvos(err,data) => {
+    queries.updateUserConvos(), (err,data) => {
       if (err) {
         console.log("error updating user convos in server:", err)
         res.send("error updating user convos in server")
       }
       console.log("user convo updated!");
       res.send(data);
-    })
+    }
   });
 
   app.get('/api/getConvo', function (req, res) {
-    queries.getConvo(err,data) => {
+    queries.getConvo(), (err,data) => {
       if (err) {
         console.log("error getting convo in server:", err)
         res.send("error getting convo in server")
       }
       console.log("convo gotten!");
       res.send(data);
-    })
+    }
   });
   //TODO: HANDLE DATA input, pass to querey
   app.get('/api/createConvo', function (req, res) {
-    queries.createConvo(err,data) => {
+    queries.createConvo(), (err,data) => {
       if (err) {
         console.log("error creating convo in server:", err)
         res.send("error creating convo in server")
       }
       console.log("convo created!");
       res.send(data);
-    })
+    }
   });
 
   app.get('/api/updateMessages', function (req, res) {
-    queries.updateMessages(err,data) => {
+    queries.updateMessages(), (err,data) => {
       if (err) {
         console.log("error updating messeges in server:", err)
         res.send("error updating messages in server")
       }
       console.log("messages updated!");
       res.send(data);
+    }
   });
 
 app.listen(port, function() {
