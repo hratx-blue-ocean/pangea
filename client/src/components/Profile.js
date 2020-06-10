9beec06a852e9567c765d27c9a6dc3f74a3dbca0
Rex-Loyer <rexloyer@gmail.com>
2020-06-10 11:01:00 -0500

    Fix Blake profile.js

---
+++ client/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calendar from './Calendar';
+const Profile = props => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  )
+}
+export default Profile;
+/*
+ /api/findUser queries by username
+/api/createUser recieves req.body
+ */
