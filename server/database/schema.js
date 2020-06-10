89693af59458635f3cf278734c68045aa389f04f
talltimblack <timothyrblack@gmail.com>
2020-06-09 23:53:02 -0500

    mvp event storage

--- server/database/schema.js
+++ server/database/schema.js
@@ -16,7 +16,7 @@ var userSchema = new mongoose.Schema({
   username: String,
   langFluent: Array,
   langInterested: Array,
-  profile: Object,
+  profile: Array,
   onlineStatus: Boolean,
   password: String,
   convoIds: Array
